379dd80fcd64d29a01cc5ebf95a79d4ecd851d5b
billahdotdev <billahdotdev@gmail.com>
2025-09-27 08:51:36 +0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import Header from './components/Header';
-import Hero from './components/Hero';
-import SEOProjects from './components/SEOProjects';
-import Expertise from './components/Expertise';
-import SocialMediaProjects from './components/SocialMediaProjects';
-import Tools from './components/Tools';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <About />
-      <Expertise />
-      <SEOProjects />
-      <SocialMediaProjects />
-      <Tools />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <h1>Oops! Blame the devs!</h1>
+    </>
   );
 }
 
